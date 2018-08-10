eae26595dd877a44990178e5e1fa0957a7c23a5e
hatelove <jay.hatelove@gmail.com>
2018-08-10 21:27:55 +0800

    extract method: IsQueryCrossMonth() and move it into Period

--- BudgetCalculator/Calculator.cs
+++ BudgetCalculator/Calculator.cs
@@ -13,6 +13,11 @@ namespace BudgetCalculator
 
         public DateTime Start { get; private set; }
         public DateTime End { get; private set; }
+
+        public bool IsQueryCrossMonth()
+        {
+            return Start.ToString("yyyyMM") != End.ToString("yyyyMM");
+        }
     }
 
     public class Calculator
@@ -40,21 +45,19 @@ namespace BudgetCalculator
 
             var budget = 0M;
 
-            var periodStart = period.Start;
-            var periodEnd = period.End;
-            if (periodStart.ToString("yyyyMM") != periodEnd.ToString("yyyyMM"))
+            if (period.IsQueryCrossMonth())
             {
-                var totalStartDaysInAMonth = DateTime.DaysInMonth(periodStart.Year, periodStart.Month);
-                var startDays = CalculateDays(periodStart, new DateTime(periodStart.Year, periodStart.Month, totalStartDaysInAMonth));
-                var startBudgetModels = budgets.Where(model => { return model.YearMonth == periodStart.ToString("yyyyMM"); });
+                var totalStartDaysInAMonth = DateTime.DaysInMonth(period.Start.Year, period.Start.Month);
+                var startDays = CalculateDays(period.Start, new DateTime(period.Start.Year, period.Start.Month, totalStartDaysInAMonth));
+                var startBudgetModels = budgets.Where(model => { return model.YearMonth == period.Start.ToString("yyyyMM"); });
                 if (startBudgetModels.Any())
                 {
                     budget += startBudgetModels.First().Budget / totalStartDaysInAMonth * startDays;
                 }
 
-                var endDays = CalculateDays(new DateTime(periodEnd.Year, periodEnd.Month, 1), periodEnd);
-                var totalEndDaysInAMonth = DateTime.DaysInMonth(periodEnd.Year, periodEnd.Month);
-                var endBudgetModels = budgets.Where(model => { return model.YearMonth == periodEnd.ToString("yyyyMM"); });
+                var endDays = CalculateDays(new DateTime(period.End.Year, period.End.Month, 1), period.End);
+                var totalEndDaysInAMonth = DateTime.DaysInMonth(period.End.Year, period.End.Month);
+                var endBudgetModels = budgets.Where(model => { return model.YearMonth == period.End.ToString("yyyyMM"); });
                 if (endBudgetModels.Any())
                 {
                     budget += endBudgetModels.First().Budget / totalEndDaysInAMonth * endDays;
@@ -63,9 +66,9 @@ namespace BudgetCalculator
                 return budget + amountOfMiddleMonths;
             }
 
-            var days = CalculateDays(periodStart, periodEnd);
-            var totalDaysInAMonth = DateTime.DaysInMonth(periodStart.Year, periodStart.Month);
-            var budgetModels = budgets.Where(model => { return model.YearMonth == periodStart.ToString("yyyyMM"); });
+            var days = CalculateDays(period.Start, period.End);
+            var totalDaysInAMonth = DateTime.DaysInMonth(period.Start.Year, period.Start.Month);
+            var budgetModels = budgets.Where(model => { return model.YearMonth == period.Start.ToString("yyyyMM"); });
             if (!budgetModels.Any())
             {
                 return 0;
