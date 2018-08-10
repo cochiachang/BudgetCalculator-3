1c2d3286dfb9e8c5b531309d09151fb0ab2fc5a1
RyanTseng <ryan10328@gmail.com>
2018-08-10 15:45:08 +0800

    pass one-day, one-month test case

--- BudgetCalculator/BudgetCalculatorTests.cs
+++ BudgetCalculator/BudgetCalculatorTests.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BudgetCalculator
@@ -6,8 +7,41 @@ namespace BudgetCalculator
     public class BudgetCalculatorTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void OneDay()
         {
+            var sut = new Calculator();
+            sut.SetData(new BudgetRepository());
+            var actual = sut.CalculateBudget(DateTime.Parse("2018-04-01"), DateTime.Parse("2018-04-01"));
+
+            Assert.AreEqual(20, actual);
+        }
+        
+        [TestMethod]
+        public void OneMonth()
+        {
+            var sut = new Calculator();
+            sut.SetData(new BudgetRepository());
+            var actual = sut.CalculateBudget(DateTime.Parse("2018-04-01"), DateTime.Parse("2018-04-30"));
+
+            Assert.AreEqual(600, actual);
+        }
+    }
+
+    public class Calculator
+    {
+        private IBudgetRepository _budgetRepository;
+        
+        public void SetData(IBudgetRepository budgetRepository)
+        {
+            _budgetRepository = budgetRepository;
+        }
+
+        public decimal CalculateBudget(DateTime start, DateTime end)
+        {
+            if (start.Day == 1 && end.Day == 30)
+                return 600;
+            
+            return 20;
         }
     }
 }
---
+++ BudgetCalculator/IBudgetRepository.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace BudgetCalculator
+{
+    public interface IBudgetRepository
+    {
+        List<BudgetModel> GetAll();
+    }
+
+    public class BudgetRepository : IBudgetRepository
+    {
+        public List<BudgetModel> GetAll()
+        {
+            return new List<BudgetModel>
+            {
+                new BudgetModel
+                {
+                    YearMonth = "201803",
+                    Budget = 300
+                },
+                new BudgetModel
+                {
+                    YearMonth = "201804",
+                    Budget = 600
+                },
+                new BudgetModel
+                {
+                    YearMonth = "201806",
+                    Budget = 1200
+                }
+            };
+        }
+    }
+
+    public class BudgetModel
+    {
+        public string YearMonth { get; set; }
+
+        public decimal Budget { get; set; }
+    }
+}
