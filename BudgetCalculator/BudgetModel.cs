afc586c1bd1e634e7fd02e97d8ad61da39790013
hatelove <jay.hatelove@gmail.com>
2018-08-11 12:05:46 +0800

    clean up Budget class

---
+++ BudgetCalculator/BudgetModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BudgetCalculator
+{
+    public class BudgetModel
+    {
+        public decimal Amount { get; set; }
+        public int Month => Convert.ToInt16(YearMonth.Substring(4, 2));
+        public int Year => Convert.ToInt16(YearMonth.Substring(0, 4));
+        public string YearMonth { get; set; }
+        private decimal DailyAmount => Amount / DaysInMonth;
+
+        private int DaysInMonth => DateTime.DaysInMonth(Year, Month);
+
+        private DateTime FirstDay => new DateTime(Year, Month, 1);
+
+        private DateTime LastDay => new DateTime(Year, Month, DaysInMonth);
+
+        public Period CreatePeriod()
+        {
+            return new Period(FirstDay, LastDay);
+        }
+
+        public decimal EffectiveTotalAmount(Period period)
+        {
+            return DailyAmount * period.OverlappingDays(CreatePeriod());
+        }
+    }
+}
--- BudgetCalculator/IBudgetRepository.cs
+++ BudgetCalculator/IBudgetRepository.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace BudgetCalculator
 {
@@ -20,52 +19,6 @@ namespace BudgetCalculator
         public void SetBudgets(List<BudgetModel> bugets)
         {
             _bugets = bugets;
-        }
-    }
-
-    public class BudgetModel
-    {
-        public string YearMonth { get; set; }
-
-        public decimal Amount { get; set; }
-
-        public int Year => Convert.ToInt16(YearMonth.Substring(0, 4));
-
-        public int Month => Convert.ToInt16(YearMonth.Substring(4, 2));
-
-        public int DaysInMonth()
-        {
-            var daysInBudgetMonth = DateTime.DaysInMonth(Year, Month);
-            return daysInBudgetMonth;
-        }
-
-        public decimal DailyAmount()
-        {
-            var dailyAmount = Amount / DaysInMonth();
-            return dailyAmount;
-        }
-
-        public DateTime FirstDay()
-        {
-            var effectiveStart = new DateTime(Year, Month, 1);
-            return effectiveStart;
-        }
-
-        public DateTime LastDay()
-        {
-            var effectiveEnd = new DateTime(Year, Month, DaysInMonth());
-            return effectiveEnd;
-        }
-
-        public Period CreatePeriod()
-        {
-            var periodFromBudget = new Period(FirstDay(), LastDay());
-            return periodFromBudget;
-        }
-
-        public decimal EffectiveTotalAmount(Period period)
-        {
-            return DailyAmount() * period.OverlappingDays(CreatePeriod());
         }
     }
 }
