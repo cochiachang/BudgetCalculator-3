3da60b9d4711946110bde6e6c8eab5221e0c661b
Claire Chang <cochia0318@hotmail.com>
2018-08-19 14:51:12 +0800

    新增預算可以取交集天數的FUNC

--- BudgetCalculator/BudgetModel.cs
+++ BudgetCalculator/BudgetModel.cs
@@ -11,6 +11,21 @@ namespace BudgetCalculator
         public int Year => int.Parse(YearMonth.Substring(0, 4));
         public int Month => int.Parse(YearMonth.Substring(4, 2));
 
-        public Period BudgetPeriod => new Period(new DateTime(Year,Month,1), new DateTime(Year,Month,DateTime.DaysInMonth(Year, Month)));
+        public decimal PerDayBudget => Budget / DaysInMonth();
+
+        public int DaysInThisBudget(Period period)
+        {
+            DateTime start = BudgetPeriod.Start > period.Start ? BudgetPeriod.Start : period.Start;
+            DateTime end = BudgetPeriod.End < period.End ? BudgetPeriod.End : period.End;
+            
+            return (end - start).Days + 1;
+        }
+
+        public Period BudgetPeriod => new Period(new DateTime(Year,Month,1), new DateTime(Year,Month,DaysInMonth()));
+
+        private int DaysInMonth()
+        {
+            return DateTime.DaysInMonth(Year, Month);
+        }
     }
 }
